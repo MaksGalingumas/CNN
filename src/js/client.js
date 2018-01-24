28811eaaf114b18c9e914e9a573ec63df3ada36c
Armandas Dambrauskas <armas_z@yahoo.com>
2018-01-24 19:38:19 +0200

    Adding project

---
+++ src/js/client.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            joke: [],
+        };
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
+    
+    componentDidMount () {
+        fetch('https://api.chucknorris.io/jokes/random')
+  .then(results => {
+    return results.json();
+  })
+  .then(data => {
+      let joke = [<p>{data.value}</p>];
+      this.setState({joke:joke});
+  })
+}
+  render () {
+    return (
+            <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12 text-center">
+                        <h2>{this.state.joke}</h2>
+                            <button onClick={this.componentDidMount} className="btn-success">NEW JOKE!</button>
+                        </div>
+                    </div>
+            </div>
+            )
+}
+}
+        
+const app = document.getElementById('app');
+ReactDOM.render(<Layout />, app);
